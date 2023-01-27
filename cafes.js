a8aefab62fa0ccd616b13ca522110797bfefd06b
Alexo416 <91638433+Alexo416@users.noreply.github.com>
2023-01-27 16:05:06 -0500

    First commit of Cafe Map

---
+++ cafes.js
@@ -0,0 +1,111 @@
+const cafe = [{
+    "NAME": "Ark + Anchor Espresso Bar",
+    "ADDRESS": "300 King St W, Hamilton, ON L8P 1B1",
+    "CATEGORY": "Medium Seating Space",
+    "LONGITUDE": -79.8781942,
+    "LATITUDE": 43.2593464,
+    "CONTENT_STRING": "<h6>Ark + Anchor Espresso Bar</h6>" +
+    "<p>Coffee, espresso & pastries served in a sparsely decorated bi-level space with a mellow vibe.</p>" +
+    "<p><strong>Address:</strong> 300 King St W, Hamilton, ON L8P 1B1</p>"
+  },
+  {
+    "NAME": "Durand Coffee",
+    "ADDRESS": "142 Charlton Ave W, Hamilton, ON L8P 2C7",
+    "CATEGORY": "Small Seating Space",
+    "LONGITUDE": -79.879541,
+    "LATITUDE": 43.251824,
+    "CONTENT_STRING": "<h6>Durand Coffee</h6>" + 
+    "<p>Mellow corner cafe for coffee, espresso & pastries served in a sparsely decorated environs.</p>" +
+    "<p><strong>Address:</strong> 142 Charlton Ave W, Hamilton, ON L8P 2C7</p>"
+  },
+  {
+    "NAME": "RELAY Coffee Roasters",
+    "ADDRESS": "27 King William St, Hamilton, ON L8R 1A1",
+    "CATEGORY": "Large Seating Space",
+    "LONGITUDE": -79.86796569824219,
+    "LATITUDE": 43.25738525390625,
+    "CONTENT_STRING": "<h6>RELAY Coffee Roasters</h6>" + 
+    "<p><strong>Address:</strong> 27 King William St, Hamilton, ON L8R 1A1</p>"
+  },
+  {
+    "NAME": "House Of Java",
+    "ADDRESS": "200 James St S, Hamilton, ON L8P 3A9",
+    "CATEGORY": "Small Seating Space",
+    "LONGITUDE": -79.86796569824219,
+    "LATITUDE": 43.25738525390625,
+    "CONTENT_STRING": "<h6>RELAY Coffee Roasters</h6>" + 
+    "<p><strong>Address:</strong> 200 James St S, Hamilton, ON L8P 3A9</p>"
+  },
+  {
+    "NAME": "Bring Your Dog Cafe",
+    "ADDRESS": "9 Richwill Rd, Hamilton, ON L9C 1R8",
+    "CATEGORY": "Large Seating Space",
+    "LONGITUDE": -79.882207,
+    "LATITUDE": 43.230681,
+    "CONTENT_STRING": "<h6>Bring Your Dog Cafe</h6>" + 
+    "<p><strong>Address:</strong> 9 Richwill Rd, Hamilton, ON L9C 1R8</p>"
+  },
+  {
+    "NAME": "Vintage Coffee Roasters",
+    "ADDRESS": "977 King St E, Hamilton, ON L8M 1C3",
+    "CATEGORY": "Small Seating Space",
+    "LONGITUDE": -79.83573913574219,
+    "LATITUDE": 43.25123977661133,
+    "CONTENT_STRING": "<h6>Vintage Coffee Roasters</h6>" + 
+    "<p><strong>Address:</strong> 977 King St E, Hamilton, ON L8M 1C3</p>"
+  },
+  {
+    "NAME": "Munchies Coffee House & Barkery",
+    "ADDRESS": "1000 Upper Gage Ave #4, Hamilton, ON L8V 4R5",
+    "CATEGORY": "Medium Seating Space",
+    "LONGITUDE": -79.84530639648438,
+    "LATITUDE": 43.213314056396484,
+    "CONTENT_STRING": "<h6>Munchies Coffee House & Barkery</h6>" + 
+    "<p><strong>Address:</strong> 1000 Upper Gage Ave #4, Hamilton, ON L8V 4R5</p>"
+  },
+  {
+    "NAME": "Paisley Coffeehouse & Eatery",
+    "ADDRESS": "1020 King St W, Hamilton, ON L8S 1L4",
+    "CATEGORY": "Small Seating Space",
+    "LONGITUDE": -79.90579986572266,
+    "LATITUDE": 43.26191711425781,
+    "CONTENT_STRING": "<h6>Paisley Coffeehouse & Eatery</h6>" + 
+    "<p><strong>Address:</strong> 1020 King St W, Hamilton, ON L8S 1L4</p>"
+  },
+  {
+    "NAME": "Williams Fresh Cafe",
+    "ADDRESS": "1309 Main St W Unit A-2, Hamilton, ON L8S 1C5",
+    "CATEGORY": "Large Seating Space",
+    "LONGITUDE": -79.91761779785156,
+    "LATITUDE": 43.25775146484375,
+    "CONTENT_STRING": "<h6>Williams Fresh Cafe</h6>" + 
+    "<p><strong>Address:</strong> 1309 Main St W Unit A-2, Hamilton, ON L8S 1C5</p>"
+  },
+  {
+    "NAME": "Starbucks",
+    "ADDRESS": "1405 Upper James St, Hamilton, ON L9B 1K2",
+    "CATEGORY": "Small Seating Space",
+    "LONGITUDE": -79.89070892333984,
+    "LATITUDE": 43.20785140991211,
+    "CONTENT_STRING": "<h6>Starbucks</h6>" + 
+    "<p><strong>Address:</strong> 1405 Upper James St, Hamilton, ON L9B 1K2</p>"
+  },
+  {
+    "NAME": "Two Cougars & A Cafe",
+    "ADDRESS": "601 Burlington St E, Hamilton, ON L8L 4J5",
+    "CATEGORY": "Large Seating Space",
+    "LONGITUDE": -79.839001,
+    "LATITUDE": 43.26754,
+    "CONTENT_STRING": "<h6>Two Cougars & A Cafe</h6>" + 
+    "<p><strong>Address:</strong> 601 Burlington St E, Hamilton, ON L8L 4J5</p>"
+  }
+
+
+
+
+
+
+
+
+
+]
